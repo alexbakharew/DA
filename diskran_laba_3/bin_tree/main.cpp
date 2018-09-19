c28a5857b381b245874bd8bca7a3fc7d02c00cd3
Alexey Bakharew <alex.bakharew@gmail.com>
2018-09-19 10:39:25 +0300

    OK on 4 lab

--- diskran_laba_3/bin_tree/main.cpp
+++ diskran_laba_3/bin_tree/main.cpp
@@ -5,16 +5,14 @@ int main()
 {
     TBtree tree;
     long val;
-    FILE* fd = fopen("01.t", "r");
 
-    for(int i = 0; i < 10000000; ++i)
+    for(int i = 0; i < 1000000; ++i)
     {
-        std::
         std::cin>>val;
         tree.Insert(val);
     }
     std::cout<<"OK\n";
-    for(int i = 0; i < 10000000; ++i)
+    for(int i = 0; i < 1000000; ++i)
     {
         std::cin>>val;
         tree.Find(val);
--- diskran_laba_3/bin_tree/test_gen.py
+++ diskran_laba_3/bin_tree/test_gen.py
@@ -1,6 +1,6 @@
 import random
 file = open("01.t", "w+")
-for i in range(20000000):
+for i in range(2000000):
     n = random.randint(-10000, 10000)
     file.write(str(n) + "\n")
 file.close()
